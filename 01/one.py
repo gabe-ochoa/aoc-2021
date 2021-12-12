d65fb6c523077ff87f3fd9fade6cb319dca115e0
Gabe <gabeochoa@gmail.com>
2021-12-11 21:07:19 -0500

    Renamefiles to make referencing easier and complete first puzzel of day 1

--- 01/one.py
+++ 01/one.py
@@ -12,3 +12,11 @@ class Day01():
                 num += 1
             previous = i
         return num
+
+
+if __name__ == '__main__':
+    print('Starting Day 1')
+    input_file = open('/code/01/input.txt', 'r')
+    data = input_file.read()
+    input_file.close()
+    print(Day01.num_increases(data))
--- 01/one_test.py
+++ 01/one_test.py
@@ -4,18 +4,18 @@ from one import Day01
 
 class Test01(unittest.TestCase):
     def test_num_instances(self):
-        input = '''199
-200
-208
-210
-200
-207
-240
-269
-260
-263'''
+        input_list = '''199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263'''
         answer = 7
-        output = Day01.num_increases(input)
+        output = Day01.num_increases(input_list)
         self.assertEqual(answer, output)
 
 
